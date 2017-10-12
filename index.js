a04bdc977e361e389ebe97843eed16170ea72e01
Nejc <nejc.rebernik1@gmail.com>
2017-10-12 11:28:56 +0200

    Added ball and keyboard controls

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ function preload() {
     game.load.image("ground", "dist/img/ground.png");
     game.load.image("sky", "dist/img/sky.png");
     game.load.image("player_square", "dist/img/player_square.png");
+    game.load.image("ball", "dist/img/ball.png");
 }
 
 function create() {
@@ -23,13 +24,41 @@ function create() {
     player = game.add.sprite(96, 0, "player_square");
     game.physics.arcade.enable(player);
     player.body.bounce.y = 0.0;
-    player.body.gravity.y = 300;
+    player.body.gravity.y = 981;
     player.body.collideWorldBounds = true;
+
+    // Ball
+    ball = game.add.sprite(450, 217, "ball");
+    game.physics.arcade.enable(ball);
+    ball.body.bounce.y = 0.37;
+    ball.body.bounce.x = 0.37;
+    ball.body.gravity.y = 981;
+    ball.body.collideWorldBounds = true;
+
+    // Keyboard controls
+    keys = game.input.keyboard.createCursorKeys();
 
 }
 
 function update() {
     // Collision detection
     var bootsOnGround = game.physics.arcade.collide(player, ground);
+    var ballOnGround = game.physics.arcade.collide(ball, ground);
+    var hitBall = game.physics.arcade.collide(player, ball);
+
+    // Reset horizontal player speed
+    player.body.velocity.x = 0;
+
+    // Check for keypresses
+    if (keys.right.isDown) {
+        player.body.velocity.x += 137;
+    }
+    if (keys.left.isDown) {
+        player.body.velocity.x += -137;
+    }
+    if (keys.up.isDown && bootsOnGround) {
+        player.body.velocity.y = -450;
+    }
+
     
 }
